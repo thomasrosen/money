fc204969a79f83fd4a325e3d7dd317b909ac149b
thomasrosen <thomas.rosen@me.com>
2023-07-05 14:56:30 +0000

    thomasrosen published a site update

--- api/index.js
+++ api/index.js
@@ -8,8 +8,11 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import { createOCRClient } from 'tesseract-wasm/node'
 import sharp from 'sharp'
+import jimp from 'jimp'
+import { cv } from 'opencv-wasm'
 
 import express from 'express'
+import cors from 'cors'
 
 import { ask_openai } from './ask_openai.js'
 
@@ -23,6 +26,31 @@ const __dirname = path.dirname(__filename);
 //   fs.mkdirSync('./cache/', { recursive: true })
 // }
 
+
+function checkOrigin(origin) {
+  let isAllowed = false
+
+  if (typeof origin === 'string') {
+    if (
+      // allow from main domain
+      origin === 'money.thomasrosen.me'
+      || origin.endsWith('://money.thomasrosen.me')
+
+      // allow from subdomains
+      || origin.endsWith('.thomasrosen.me')
+
+      // allow for localhost
+      || origin.endsWith('localhost:3000')
+      || origin.endsWith('localhost:13151')
+      || origin.endsWith('0.0.0.0:3000')
+      || origin.endsWith('0.0.0.0:13151')
+    ) {
+      isAllowed = true
+    }
+  }
+
+  return isAllowed
+}
 
 async function ocr_result_to_structured_json (ocr_result_text) {
   console.info('sending to gpt-3')
@@ -80,6 +108,72 @@ Be precise with all the values.
   return null
 }
 
+/*
+async function image_to_foreground(sharp_image) {
+  try {
+  const photoBuffer = await jimp.read(await sharp_image.toBuffer())
+  const src = cv.matFromImageData(photoBuffer.bitmap)
+
+  // const jimpBuffer = await jimp.read(image_buffer)
+  // const src = cv.matFromImageData(jimpBuffer.bitmap)
+    console.log('src', src)
+  cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
+  let mask = new cv.Mat();
+  let bgdModel = new cv.Mat();
+  let fgdModel = new cv.Mat();
+  let rect = new cv.Rect(50, 50, 260, 280);
+    console.log('rect', rect)
+  cv.grabCut(src, mask, rect, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_RECT);
+  console.log('after grab cut')
+  // draw foreground
+  for (let i = 0; i < src.rows; i++) {
+    console.log('i', i)
+    for (let j = 0; j < src.cols; j++) {
+      if (mask.ucharPtr(i, j)[0] === 0 || mask.ucharPtr(i, j)[0] === 2) {
+        src.ucharPtr(i, j)[0] = 0;
+        src.ucharPtr(i, j)[1] = 0;
+        src.ucharPtr(i, j)[2] = 0;
+      }
+    }
+  }
+  // draw grab rect
+  let color = new cv.Scalar(0, 0, 255);
+  let point1 = new cv.Point(rect.x, rect.y);
+  let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
+    console.log('point2', point2)
+  cv.rectangle(src, point1, point2, color);
+  // cv.imshow('canvasOutput', src);
+  console.log('src', src)
+    const foreground_buffer = Buffer.from(src) // convert to buffer
+    console.log('foreground_buffer', foreground_buffer)
+
+  // clean up
+  src.delete();
+  mask.delete();
+  bgdModel.delete();
+  fgdModel.delete();
+
+    return foreground_buffer
+
+} catch (error) {
+  console.error('f-error', error)
+}
+
+return null
+}
+*/
+
+async function save_debug_image(sharp_image, filename) {
+  // save image to disk for debugging
+  // checkif ./images/ exists
+  // use_cache()
+  if (!fs.existsSync('./cache/images/')) {
+    fs.mkdirSync('./cache/images/', { recursive: true })
+  }
+  await sharp_image.toFile(`./cache/images/debug_${filename}.png`)
+  console.log('saved debug image')
+}
+
 async function loadImage(buffer) {
   // I use the idea from the following answer to remove the shadows: https://stackoverflow.com/questions/44047819/increase-image-brightness-without-overflow/44054699#44054699
 
@@ -92,6 +186,9 @@ async function loadImage(buffer) {
       [0.333, 0.333, 0.333],
     ])
     .normalise() // full range 0 to 255
+  console.log('checked rotation of image + converted to grayscale')
+
+
 
   let { width, height, orientation } = await image_bw.metadata()
   console.info('width, height, orientation', width, height, orientation)
@@ -104,10 +201,44 @@ async function loadImage(buffer) {
     height = tmp
   }
 
-  const image_blurred = await sharp(await image_bw.clone().toBuffer())
-    .median(61) // median-blur // 30px is the line-height sweet-spot for tesseract. so 61px as a median blur should remove most of the lines
-    .blur(1) // TODO is this extra blur necessary?
+
+
+
+  // const foreground_buffer = await image_to_foreground(image_bw)
+  // console.log('foreground_buffer', foreground_buffer)
+  // const foreground_buffer_image = await sharp(foreground_buffer, {
+  //   raw: {
+  //     width,
+  //     height,
+  //     channels: 4,
+  //   }
+  // })
+  // console.log('got foreground sharp image')
+
+
+  // throw new Error('stop here')
+
+  // START dialate
+  const photoBuffer = await jimp.read(await image_bw.toBuffer())
+  const src = cv.matFromImageData(photoBuffer.bitmap)
+  let dst = new cv.Mat()
+  let M = cv.Mat.ones(7, 7, cv.CV_8U) // 7x7 kernel with all 1's
+  let anchor = new cv.Point(-1, -1)
+  cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue())
+  const image_dilated = Buffer.from(dst.data)
+  console.log('image dilated')
+  // END dialate
+
+  const image_blurred = await sharp(image_dilated, {
+    raw: {
+      width,
+      height,
+      channels: 4,
+    }
+  }) // await sharp(await image_bw.clone().toBuffer())
+    .median(21) // median-blur // 30px is the line-height sweet-spot for tesseract. so 61px as a median blur should remove most of the lines BUT the stackoverflow-article suggests 21px as a good value. i guess this is 3x the kernel size of the dialation
     .normalise()
+  console.log('image blurred')
 
   const bw_buffer = await image_bw.extractChannel('red').raw().toBuffer()
 
@@ -127,6 +258,7 @@ async function loadImage(buffer) {
       }
       return rgb
     })
+  console.log('removed shadows from image')
 
   const image_better = await sharp(Buffer.from(new_grayscale), {
     raw: {
@@ -140,13 +272,19 @@ async function loadImage(buffer) {
     .ensureAlpha() // add alpha channel if not already present
 
 
-  // // save image to disk for debugging
-  // // checkif ./images/ exists
+
+
+
+
+
+  // save image to disk for debugging
+  // checkif ./images/ exists
   // use_cache()
-  // if (!fs.existsSync('./cache/images/')) {
-  //   fs.mkdirSync('./cache/images/', { recursive: true })
-  // }
-  // await image_better.toFile(`./cache/images/debug.png`)
+  if (!fs.existsSync('./cache/images/')) {
+    fs.mkdirSync('./cache/images/', { recursive: true })
+  }
+  await image_better.toFile(`./cache/images/debug.png`)
+  console.log('saved debug image')
 
   return {
     data: await image_better.raw().toBuffer(),
@@ -207,18 +345,31 @@ async function readRequestBody(request) {
 console.info('Initializing server...')
 
 const app = express()
+app.use(cors())
+app.use(function (req, res, next) {
+  console.log('app.use - request url:', req.url)
+
+  // const origin = req.get('origin')
+  const origin = req.header('Origin')
+  if (checkOrigin(origin)) {
+    req.is_subdomain = true
+    req.origin = origin
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  } else {
+    req.is_subdomain = false
+  }
+
+  next()
+})
+
+app.use('/', express.static('../build/', { fallthrough: true }))
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/item_id`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end('The api is under /api/ocr')
+})
 
 
 
@@ -254,6 +405,8 @@ async function get_ocr_client(options) {
 }
 
 app.post('/api/ocr', async (req, res) => {
+  console.log('/api/ocr')
+
 
   const client = await get_ocr_client({
     lang: 'deu',
@@ -306,7 +459,10 @@ app.post('/api/ocr', async (req, res) => {
 const static_files_path = path.join(__dirname, '../build/')
 app.use(express.static(static_files_path))
 
-
+app.get('*', (req, res) => {
+  // console.log('index.html fallcack - request url:', req.url)
+  res.sendFile('index.html', { root: '../build/' })
+})
 
 const port = process.env.PORT || 13151 // the word money as its letter positions in the abc = 13 15 14 5 25
 const host = '0.0.0.0' // Uberspace wants 0.0.0.0 instead of localhost
--- src/components/pages/upload.js
+++ src/components/pages/upload.js
@@ -5,36 +5,49 @@ const thumbsContainer = {
   display: 'flex',
   flexDirection: 'row',
   flexWrap: 'wrap',
-  marginTop: 16
-};
+  marginTop: '16px',
+}
 
 const thumb = {
-  display: 'inline-flex',
-  borderRadius: 2,
   border: '1px solid #eaeaea',
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: 'border-box'
-};
-
-const thumbInner = {
-  display: 'flex',
-  minWidth: 0,
-  overflow: 'hidden'
-};
+  marginBottom: '8px',
+  marginRight: '8px',
+  width: 'auto',
+  height: '100px',
+  padding: '4px',
+}
 
 const img = {
   display: 'block',
   width: 'auto',
-  height: '100%'
-};
+  height: '100%',
+}
+
+// function file_to_image(file) {
+//   return new Promise((resolve, reject) => {
+//     const reader  = new FileReader()
+//     // it's onload event and you forgot (parameters)
+//     reader.onload = function(e)  {
+//         console.log('e.target.result', e.target.result)
+//         var image = document.createElement('img')
+//         // the result image data
+//         image.src = e.target.result
+//       document.body.appendChild(image)
+//       console.log('image', image)
+//         resolve()
+//      }
+//      reader.onerror = function(e) {
+//       reject()
+//      }
+//      // you have to declare the file loading
+//      reader.readAsDataURL(file)
+//   })
+//  }
 
 export default function Upload() {
 
   const [files, setFiles] = useState([])
+  const [result, setResult] = useState(null)
 
   const onChange = useCallback(acceptedFiles => {
     acceptedFiles = acceptedFiles.map(file => Object.assign(file, {
@@ -44,37 +57,77 @@ export default function Upload() {
     setFiles(acceptedFiles)
   }, [])
 
-  return <div>
-    <Dropzone onChange={onChange} />
-    <br />
-    <br />
-
-    {
-      files.length > 0
-      ? <>
-        <h4>Files</h4>
-        <aside style={thumbsContainer}>
-        {
-          files
-          .map(file => (
-            <div style={thumb} key={file.name}>
-              <div style={thumbInner}>
-                <img
-                  alt={`${file.path} - ${file.size} bytes`}
-                  title={`${file.path} - ${file.size} bytes`}
-                  src={file.preview}
-                  style={img}
-                  // Revoke data uri after image is loaded
-                  onLoad={() => { URL.revokeObjectURL(file.preview) }}
-                />
-              </div>
-            </div>
-          ))
-        }
-        </aside>
-      </>
-      : null
+  const send_files = useCallback(async () => {
+    if (files.length === 0) {
+      throw new Error('No files selected')
     }
 
+    const file = files[0]
+    console.log('file', file)
+
+    // const data_url = file_to_image(file)
+
+    // get image-blob-content from the file
+    // const imageResponse = await fetch(file.preview)
+    // const imageBlob = await imageResponse.blob()
+
+    // create a formdata object
+    // const formData = new FormData()
+    // formData.append('file', file)
+
+    // send the formdata to the server at /api/ocr using fetch
+    const response = await fetch(`${window.urls.api}ocr`, {
+      method: 'POST',
+      body: file, // formData
+    })
+    const json = await response.json()
+    setResult(json)
+
+
+    // setResult({
+    //   hello: 'world'
+    // })
+
+  }, [files])
+
+  return <div>
+    <div className="middle_box">
+      <Dropzone onChange={onChange} />
+      <br />
+      {
+        files.length > 0
+        ? <>
+          <h4>Selected Images</h4>
+          <aside style={thumbsContainer}>
+          {
+            files
+              .map(file => (
+            <div style={thumb} key={file.name}>
+              <img
+                alt={`${file.path} - ${file.size} bytes`}
+                title={`${file.path} - ${file.size} bytes`}
+                src={file.preview}
+                style={img}
+                // Revoke data uri after image is loaded
+                onLoad={() => { URL.revokeObjectURL(file.preview) }}
+              />
+            </div>
+              ))
+          }
+          </aside>
+          <button onClick={send_files}>Send one file</button>
+        </>
+        : null
+      }
+      {
+        result !== null
+          ? <>
+            <br />
+            <br />
+            <pre><code>{JSON.stringify(result,null,2)}</code></pre>
+          </>
+          : null
+      }
+    </div>
   </div>
 }
