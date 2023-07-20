9930126971602db42fd149053dde9e8979c8714d
thomasrosen <thomas.rosen@me.com>
2023-07-20 13:43:56 +0200

    started the inoice-item editor

--- src/components/pages/Editor.js
+++ src/components/pages/Editor.js
@@ -12,6 +12,8 @@ import {
   empty_invoice,
 } from '../../redux/slices/invoicesSlice.js'
 
+import { v4 as uuidv4 } from 'uuid'
+
 const currency_label = '€ / $ / …'
 const labels = {
   date_created: 'Date Created',
@@ -40,7 +42,7 @@ const editor_types = {
   amount_of_tip: 'money',
   // amount_of_tip_currency: 'string',
   way_of_payment: 'string',
-  items: 'ids',
+  items: 'item',
   images: 'ids',
 }
 
@@ -63,9 +65,129 @@ function Input ({ label, ...props }) {
   />
 }
 
+function MoneyAmountInput({
+  label,
+  amount,
+  currency,
+  currency_options = '€$',
+  onAmountChange,
+  onCurrencyChange,
+}) {
+  const currency_label = '€ / $ / …'
+  return <div style={{ display: 'flex', gap: '10px' }}>
+    <Input
+      style={{ width: '100px' }}
+      label={currency_label}
+      inputValue={currency}
+      options={
+        currency_options
+        .split('')
+        .map(title => ({
+          title: title,
+          firstLetter: (
+            typeof title === 'string' && title.length > 0
+              ? title[0].toUpperCase()
+              : '?'
+          )
+        }))
+        .sort((a, b) => -b.title.localeCompare(a.title))
+      }
+      getOptionLabel={option => option.title || ''}
+      // groupBy={option => option.firstLetter || '?'}
+      onInputChange={(_, newValue) => {
+        onCurrencyChange(newValue)
+      }}
+    />
+    <TextField
+      fullWidth
+      label={label}
+      InputProps={{
+        // type: 'number',
+        // step: '0.01',
+        inputMode: 'numeric',
+        pattern: '[0-9,.]*',
+      }}
+      defaultValue={amount}
+      onChange={event => {
+        const newValue = event.target.value
+        onAmountChange(newValue)
+      }}
+    />
+  </div>
+}
+
+function ItemEditor ({
+  items = [],
+  style,
+  deafultValue,
+  onChange,
+}) {
+  const [value, setValue] = useState(deafultValue || {})
+
+  function get_item_options (items, key) {
+    return [...new Set(items
+      .map(option => String(option[key] || ''))
+      .filter(Boolean)
+    )]
+      .map(title => ({
+        title: title,
+        firstLetter: (
+          typeof title === 'string' && title.length > 0
+            ? title[0].toUpperCase()
+            : '?'
+        )
+      }))
+      .sort((a, b) => -b.title.localeCompare(a.title))
+  }
+
+  return <div style={style}>
+
+    <Input
+      label="Item Name"
+      inputValue={value.title || ''}
+      options={get_item_options(items, 'title')}
+      getOptionLabel={option => option.title || ''}
+      groupBy={option => option.firstLetter || '?'}
+      onInputChange={(_, newTitle) => {
+        const newValue = {
+          ...value,
+          title: newTitle,
+        }
+        setValue(newValue)
+        onChange(newValue)
+      }}
+    />
+
+    <MoneyAmountInput
+      label="Price of Item"
+      amount={value?.amount || ''}
+      currency={value?.currency || ''}
+      onAmountChange={newAmount => {
+        const newValue = {
+          ...value,
+          amount: newAmount,
+        }
+        setValue(newValue)
+        onChange(newValue)
+      }}
+      onCurrencyChange={newCurrency => {
+        const newValue = {
+          ...value,
+          currency: newCurrency,
+        }
+        setValue(newValue)
+        onChange(newValue)
+      }}
+    />
+  </div>
+}
+
 export default function Editor() {
 
-  const [value, setValue] = useState(empty_invoice);
+  const [invoice, setInvoice] = useState({
+    ...empty_invoice,
+    id: uuidv4(),
+  });
 
   const dispatch = useDispatch()
   const invoices = useSelector(selectInvoices)
@@ -79,8 +201,8 @@ export default function Editor() {
     <br />
 
     <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-    {
-      Object.keys(value).map(key => {
+      {
+      Object.keys(invoice).map(key => {
         const label = labels[key] || key
 
         if (editor_types[key] === 'number') {
@@ -94,11 +216,11 @@ export default function Editor() {
                 inputMode: 'numeric',
                 pattern: '[0-9,.]*',
               }}
-              defaultValue={value[key] || ''}
+              defaultValue={invoice[key] || ''}
               onChange={event => {
                 const newValue = event.target.value
-                setValue({
-                  ...value,
+                setInvoice({
+                  ...invoice,
                   [key]: newValue || '',
                 })
               }}
@@ -122,12 +244,12 @@ export default function Editor() {
                 type: 'datetime-local',
                 startAdornment: <InputAdornment style={{ margin: '0' }} position="start"></InputAdornment>,
               }}
-              defaultValue={value[key] || now}
+              defaultValue={invoice[key] || now}
               onChange={event => {
                 const newValue = event.target.value
                 console.log('newValue', newValue)
-                setValue({
-                  ...value,
+                setInvoice({
+                  ...invoice,
                   [key]: newValue || '',
                 })
               }}
@@ -137,56 +259,80 @@ export default function Editor() {
 
         if (editor_types[key] === 'money') {
           const key_currency = key + '_currency'
-          const label_currency = labels[key_currency] || key_currency
+          return <MoneyAmountInput
+            key={key}
+            label={label}
+            amount={invoice[key] || ''}
+            currency={invoice[key_currency] || ''}
+            onAmountChange={newValue => {
+              setInvoice({
+                ...invoice,
+                [key]: newValue || '',
+              })
+            }}
+            onCurrencyChange={newValue => {
+              setInvoice({
+                ...invoice,
+                [key_currency]: newValue || '',
+              })
+            }}
+          />
+        }
 
-          return <div key={key} style={{ display: 'flex', gap: '10px' }}>
-            <Input
-              style={{ width: '100px' }}
-              label={label_currency}
-              inputValue={value[key_currency] || ''}
-              options={
-                [...new Set(invoices
-                  .map(option => String(option[key_currency] || ''))
-                  .filter(Boolean)
-                )]
-                  .map(title => ({
-                    title: title,
-                    firstLetter: (
-                      typeof title === 'string' && title.length > 0
-                        ? title[0].toUpperCase()
-                        : '?'
-                    )
-                  }))
-                  .sort((a, b) => -b.title.localeCompare(a.title))
-              }
-              getOptionLabel={option => option.title || ''}
-              groupBy={option => option.firstLetter || '?'}
-              onInputChange={(_, newValue) => {
-                console.log(newValue);
-                setValue({
-                  ...value,
-                  [key_currency]: newValue || '',
+        if (editor_types[key] === 'item') {
+          const items = (invoice[key] || [])
+          return <div key={key}>
+            <br />
+            <h3>Items</h3>
+            {
+              items?.map((item, index) => {
+                return <ItemEditor
+                  style={{
+                    marginBlockStart: '16px',
+                    marginBlockEnd: '16px',
+                  }}
+                  key={item.id || index}
+                  deafultValue={item}
+                  onChange={newValue => {
+                    console.log('ItemEditor onchange', newValue)
+
+                    const new_items = items
+                    const index = new_items.findIndex(item => item.id === newValue.id)
+                    if (index !== -1) {
+                      new_items[index] = newValue
+                    } else {
+                      new_items.push(newValue)
+                    }
+
+                    setInvoice({
+                      ...invoice,
+                      [key]: new_items,
+                    })
+                  }}
+                />
+              })
+            }
+            <br />
+            <button
+              className="primary"
+              onClick={() => {
+                const new_value = [
+                  ...items,
+                  {
+                    id: uuidv4()
+                  }
+                ]
+
+                setInvoice({
+                  ...invoice,
+                  [key]: new_value,
                 })
               }}
-            />
-            <TextField
-              fullWidth
-              label={label}
-              InputProps={{
-                // type: 'number',
-                // step: '0.01',
-                inputMode: 'numeric',
-                pattern: '[0-9,.]*',
-              }}
-              defaultValue={value[key] || ''}
-              onChange={event => {
-                const newValue = event.target.value
-                setValue({
-                  ...value,
-                  [key]: newValue || '',
-                })
-              }}
-            />
+            >
+              Add Item
+            </button>
+            <br />
+            <br />
           </div>
         }
 
@@ -201,7 +347,7 @@ export default function Editor() {
         return <div key={key}>
           <Input
             label={label}
-            inputValue={value[key] || ''}
+            inputValue={invoice[key] || ''}
             options={
               [...new Set(invoices
                 .map(option => String(option[key] || ''))
@@ -221,8 +367,8 @@ export default function Editor() {
             groupBy={option => option.firstLetter || '?'}
             onInputChange={(_, newValue) => {
               console.log(newValue);
-              setValue({
-                ...value,
+              setInvoice({
+                ...invoice,
                 [key]: newValue || '',
               })
             }}
@@ -230,17 +376,17 @@ export default function Editor() {
         </div>
       })
       .filter(Boolean)
-    }
+      }
 
-    <button
-      className="primary"
-      onClick={() => {
-        console.log('value', value)
-          dispatch(addInvoices(value))
-      }}
-    >
-      Add
-    </button>
+      <button
+        className="primary"
+        onClick={() => {
+          console.log('invoice', invoice)
+          dispatch(addInvoices([invoice]))
+        }}
+      >
+        Save Invoice
+      </button>
     </div>
 
   </div>
